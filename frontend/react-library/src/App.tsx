4c52c1dcb2ef3f0a1b1f03564b898897be24f91f
sidharth1211 <communicate.sidharth@gmail.com>
2023-04-17 12:24:09 +0530

    First Commit

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+import { HomePage } from "./layouts/Homepage/HomePage";
+
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <HomePage /> */}
+      <SearchBooksPage/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
